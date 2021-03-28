6bb9a4c2f10807b2dedbd3e18c2b5327f777cbb9
shehabkhalid <shehabkhalid1999@gmail.com>
2021-03-28 23:22:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
-import Editor from "./components/Editor";
+import React from 'react'
+//import Editor from "./components/Editor";
+import Editor, { useMonaco } from "@monaco-editor/react";
+
+import { listen } from 'vscode-ws-jsonrpc';
+
+import
+{
+  MonacoLanguageClient, CloseAction, ErrorAction,
+  MonacoServices, createConnection,
+} from 'monaco-languageclient';
+import ReconnectingWebSocket from 'reconnecting-websocket';
 function App()
 {
+
+  const monaco = useMonaco();
+  React.useEffect(() =>
+  {
+    if (monaco)
+    {
+
+      monaco.languages.register({
+        id: 'python',
+        extensions: ['.py'],  
+        aliases: ['python'],
+        mimetypes: ['application/text']
+      },{
+        id: 'cpp',
+        extensions: ['.cpp'],  
+        aliases: ['cpp'],
+        mimetypes: ['application/text']
+      })
+      //monaco.editor.getModel(monaco.Uri.parse("inmemory://dummy.py"))||
+     // monaco.editor.createModel('', 'python', monaco.Uri.parse('inmemory://dummy.py'))
+     // monaco.editor.createModel('', 'c', monaco.Uri.parse('/src/test.cpp'))
+      //const urll = monaco.Uri.parse('file:///demo/ts')
+
+      MonacoServices.install(monaco);
+      // MonacoServices.install(require('monaco-editor-core/esm/vs/platform/commands/common/commands').CommandsRegistry);
+      const url = 'ws://localhost:3003/python'
+      const webSocket = createWebSocket(url);
+
+      listen({
+        webSocket,
+        onConnection: connection =>
+        {
+          // create and start the language client
+          const languageClient = createLanguageClient(connection);
+          const disposable = languageClient.start();
+          connection.onClose(() => disposable.dispose());
+        }
+      });
+
+
+      function createLanguageClient(connection)
+      {
+        return new MonacoLanguageClient({
+          name: "Sample Language Client",
+          clientOptions: {
+            // use a language id as a document selector
+            documentSelector: ['python','cpp'],
+            // disable the default error handler
+            errorHandler: {
+              error: () => ErrorAction.Continue,
+              closed: () => CloseAction.DoNotRestart
+            }
+          },
+          // create a language client connection from the JSON RPC connection on demand
+          connectionProvider: {
+            get: (errorHandler, closeHandler) =>
+            {
+              return Promise.resolve(createConnection(connection, errorHandler, closeHandler))
+            }
+          }
+        });
+      }
+
+
+      function createWebSocket(url)
+      {
+        const socketOptions = {
+          maxReconnectionDelay: 10000,
+          minReconnectionDelay: 1000,
+          reconnectionDelayGrowFactor: 1.3,
+          connectionTimeout: 10000,
+          maxRetries: Infinity,
+          debug: false
+        };
+        return new ReconnectingWebSocket(url, [], socketOptions);
+      }
+
+
+      console.log("here is the monaco isntance:", monaco.languages);
+    }
+  }, [monaco]);
+
+
   return (
     <div>
-     
-      <Editor/>
-     
+
+      <h1>GP editor monaco with LSP and React test 56 (python)</h1>
+
+      <Editor
+        height="90vh"
+        defaultLanguage="python"
+        
+       // path="file:///GP_C/test.cc"
+        theme="vs-dark"
+      />
+
     </div>
   );
 }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,89 +1,108 @@
 import React from 'react'
-import * as monaco from 'monaco-editor'
-import { listen} from '@codingame/monaco-jsonrpc';
-const {
-    MonacoLanguageClient, CloseAction, ErrorAction,
-    MonacoServices, createConnection
-} = require('monaco-languageclient/lib');
+import * as monaco from 'monaco-editor-core'
+import { listen } from 'vscode-ws-jsonrpc';
+import
+    {
+        MonacoLanguageClient, CloseAction, ErrorAction,
+        MonacoServices, createConnection
+    } from 'monaco-languageclient';
 import ReconnectingWebSocket from 'reconnecting-websocket';
-const Editor = () => {
+const Editor = () =>
+{
 
     let divNode;
-    const assignRef = React.useCallback((node) => {
+    const assignRef = React.useCallback((node) =>
+    {
         // On mount get the ref of the div and assign it the divNode
         divNode = node;
     }, []);
 
-    
-    React.useEffect(() => {
-        if (divNode) {
-            
+
+    React.useEffect(() =>
+    {
+      //  MonacoServices.install(require('monaco-editor-core/esm/vs/platform/commands/common/commands').CommandsRegistry);
+
+        if (divNode)
+        {
 
 
 
-
+            const value = `{
+                "$schema": "http://json.schemastore.org/coffeelint",
+                "line_endings": "unix"
+            }`;
             monaco.languages.register({
-                id: 'typescript',
-                extensions: ['.ts'],
-                aliases: ['TypeScript','ts','TS','Typescript','typescript']
-            })
+                id: 'json',
+                extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
+                aliases: ['JSON', 'json'],
+                mimetypes: ['application/json'],
+            });
 
             const editor = monaco.editor.create(divNode, {
-                language: 'javascript',
+                model: monaco.editor.createModel(value, 'json', monaco.Uri.parse('inmemory://model.json')),
+
+                language: 'typescript',
                 minimap: { enabled: true },
                 autoIndent: true,
-                theme:"vs-dark"
+                theme: "vs-dark"
             });
 
 
-            // const url = 'ws://localhost:3000/ts'
-            // const webSocket = createWebSocket(url);
+         
+         
+            MonacoServices.install(require('monaco-editor/esm/vs/platform/commands/common/commands').CommandsRegistry);
+            // MonacoServices.install(editor,{rootUri: "file:///D:/small projects/GP-stuff/demo/ts"});
+            const url = 'ws://localhost:3001/json'
+            const webSocket = createWebSocket(url);
 
-            // listen({
-            //     webSocket,
-            //     onConnection: connection => {
-            //         // create and start the language client
-            //         const languageClient = createLanguageClient(connection);
-            //         const disposable = languageClient.start();
-            //         connection.onClose(() => disposable.dispose());
-            //     }
-            // });
-
-
-
-            // function createLanguageClient(connection) {
-            //     return new MonacoLanguageClient({
-            //         name: "Sample Language Client",
-            //         clientOptions: {
-            //             // use a language id as a document selector
-            //             documentSelector: ['typescript'],
-            //             // disable the default error handler
-            //             errorHandler: {
-            //                 error: () => ErrorAction.Continue,
-            //                 closed: () => CloseAction.DoNotRestart
-            //             }
-            //         },
-            //         // create a language client connection from the JSON RPC connection on demand
-            //         connectionProvider: {
-            //             get: (errorHandler, closeHandler) => {
-            //                 return Promise.resolve(createConnection(connection, errorHandler, closeHandler))
-            //             }
-            //         }
-            //     });
-            // }
+            listen({
+                webSocket,
+                onConnection: connection =>
+                {
+                    // create and start the language client
+                    const languageClient = createLanguageClient(connection);
+                    const disposable = languageClient.start();
+                    connection.onClose(() => disposable.dispose());
+                }
+            });
 
 
-            // function createWebSocket(url) {
-            //     const socketOptions = {
-            //         maxReconnectionDelay: 10000,
-            //         minReconnectionDelay: 1000,
-            //         reconnectionDelayGrowFactor: 1.3,
-            //         connectionTimeout: 10000,
-            //         maxRetries: Infinity,
-            //         debug: false
-            //     };
-            //     return new ReconnectingWebSocket(url, [], socketOptions);
-            // }
+            function createLanguageClient(connection)
+            {
+                return new MonacoLanguageClient({
+                    name: "Sample Language Client",
+                    clientOptions: {
+                        // use a language id as a document selector
+                        documentSelector: ['typescript'],
+                        // disable the default error handler
+                        errorHandler: {
+                            error: () => ErrorAction.Continue,
+                            closed: () => CloseAction.DoNotRestart
+                        }
+                    },
+                    // create a language client connection from the JSON RPC connection on demand
+                    connectionProvider: {
+                        get: (errorHandler, closeHandler) =>
+                        {
+                            return Promise.resolve(createConnection(connection, errorHandler, closeHandler))
+                        }
+                    }
+                });
+            }
+
+
+            function createWebSocket(url)
+            {
+                const socketOptions = {
+                    maxReconnectionDelay: 10000,
+                    minReconnectionDelay: 1000,
+                    reconnectionDelayGrowFactor: 1.3,
+                    connectionTimeout: 10000,
+                    maxRetries: Infinity,
+                    debug: false
+                };
+                return new ReconnectingWebSocket(url, [], socketOptions);
+            }
 
         }
     }, [assignRef])
