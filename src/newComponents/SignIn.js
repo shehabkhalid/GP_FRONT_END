be0825347aa53f3fc61e2bb030f8b39ae64ee093
Abdullah-Baher <62217391+Abdullah-Baher@users.noreply.github.com>
2021-07-08 03:53:25 +0200

    api functions added

--- src/newComponents/SignIn.js
+++ src/newComponents/SignIn.js
@@ -1,18 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import usersApi from "../API/user";
 
 class SignIn extends Component {
+  
+ constructor(props) {
+   super(props);
+   this.emailRef = createRef();
+   this.passwordRef = createRef();
+ }
+
+ checkUser = async (e) => {
+   e.preventDefault();
+   const email = this.emailRef.current.value;
+   const password = this.passwordRef.current.value;
+
+   if(email && password) {
+        const response = await usersApi.loginUser({ email, password });
+        console.log(response);
+   }
+ }
+  
   render() {
     return (
-      <Form className="p-5 bg-light">
+      <Form className="p-5 bg-light" onSubmit={this.checkUser}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             className="shadow-sm rounded-0"
             type="email"
             placeholder="Enter email"
+            ref={this.emailRef}
           />
+
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -24,10 +45,11 @@ class SignIn extends Component {
             className="shadow-sm rounded-0"
             type="password"
             placeholder="Password"
+            ref={this.passwordRef}
           />
         </Form.Group>
 
-        <Link to="/default/home">
+       {/*<Link to="/default/home">*/}
           <Button
             className="shadow-sm rounded-0"
             variant="primary"
@@ -35,7 +57,7 @@ class SignIn extends Component {
           >
             Submit
           </Button>
-        </Link>
+        {/*</Link>*/}
       </Form>
     );
   }
