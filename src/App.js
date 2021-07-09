35056e55a0bcd99e11eef4a19392ce332b293b7d
shehabkhalid <shehabkhalid1999@gmail.com>
2021-07-09 04:04:12 +0200

    araff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./newComponents/Main";
 import Default from "./newComponents/Default";
-import Terminal  from './components/Terminal'
+
 import "./App.css";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Terminal} >
-
-          </Route>
+          <Route exact path="/" component={Main} />
+          
           <Route path="/default" component={Default} />
-          {/* <Route path="/araf">
-            <Terminal runner={true} />
-          </Route> */}
+     
         </Switch>
       </BrowserRouter>
     );
